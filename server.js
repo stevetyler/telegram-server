1322a776dcdf4fc9cd015cd5d5db41f448137555
Steve Tyler <mail@steve-tyler.co.uk>
2014-09-29 23:02:40 +0100

    Added 'operation' property and updated users route

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+
+app.use(bodyParser.json());
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
@@ -9,17 +13,20 @@ var users = [{
         id: 'stevetyler',
         name: 'Steve Tyler',
         password: 'steve',
-        imageURL: '/assets/img/users/stevetyler.png'
+        imageURL: '/assets/img/users/stevetyler.png',
+        operation: 'login'
     }, {
         id: 'vivhoford',
         name: 'Vivien Hoford',
         password: 'vivien',
-        imageURL: '/assets/img/users/vivhoford.png'
+        imageURL: '/assets/img/users/vivhoford.png',
+        operation: 'login'
     }, {
         id: 'rachelblanton',
         name: 'Rachel Blanton',
         password: 'rachel',
-        imageURL: '/assets/img/users/rachelblanton.png'
+        imageURL: '/assets/img/users/rachelblanton.png',
+        operation: 'login'
     }];
 
 var posts = [{
@@ -40,11 +47,33 @@ var posts = [{
         body: 'I have no idea what you\'re talking about',
     }];
     
-
-
 // Route implementation
 app.get('/api/users', function(req, res) {
-  res.status(200).send({'users': users});
+    var operation = req.query.operation;
+    var username = req.query.username;
+    var password = req.query.password;
+    var user;
+
+    if (operation === 'login') {
+
+        // search user and check password matches, then send back array that only contains that user to client.
+        for (var i = 0; i < users.length; i++) {
+            if (users[i]['id'] === username) {
+                user = users[i];
+            }
+        }
+        if (password === user.password) {
+            console.log(user.password);
+            console.log(password);
+            res.status(200).send({'users': [user]});
+        }
+        else {
+            res.status(200).send({'users': []});
+        }
+    }
+    else {
+        res.status(200).send({'users': users});
+    }
 });
 
 app.get('/api/posts', function(req, res) {
@@ -59,5 +88,25 @@ app.get('/api/users/:id', function(req, res) {
         }
     }
 });
+
+app.post('/api/users', function(req, res) {
+   users.push(req.body.user);
+   res.send({'user': req.body.user});
+});
+
+app.post('/api/posts', function(req, res) {
+    var post = {
+        id: posts.length + 1,
+        user: req.body.post.user,
+        createdDate: req.body.post.createdDate,
+        body: req.body.post.body
+    };
+    console.log(post);
+    posts.push(post);
+    res.send({'post': post});
+});
+
+
+
 
 
