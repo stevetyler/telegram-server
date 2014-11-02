67c77dd2959a724e4b1661dd18b7a81dbdc79079
Steve Tyler <mail@steve-tyler.co.uk>
2014-11-02 18:24:04 +0000

    Implemented connect-mongostore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var session = require('express-session');
+var MongoStore = require('connect-mongostore')(session);
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var passport = require('passport');
@@ -49,7 +50,8 @@ app.use(session({
     secret: 'keyboard cat', // what is this??
     resave: true,  // forces session to be saved even when unmodified
     saveUninitialized: true,  // forces a new unmodified session to be saved to the store. 
-    rolling: false  // reset expiration date setting cookie on every response
+    rolling: false,  // reset expiration date setting cookie on every response
+    store: new MongoStore({'db': 'sessions'})
 }));
 app.use(passport.initialize());
 app.use(passport.session());
