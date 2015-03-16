a54b54fc6921f5b2feef7352d6e107271e325661
Steve Tyler <mail@steve-tyler.co.uk>
2015-03-16 18:29:01 +0000

    Twitter auth module

---
+++ auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'twitterAuth' : {
+		'consumerKey': 'qV8AhrTENneEbMaiy8MEOHWfi',
+		'consumerSecret': 'FH0sf3agdNQppopS1ilBHrZXOVPcgH9uBhhWxhq67r4VLlXLa3',
+		'callbackURL': 'http://178.62.92.55/auth/twitter/callback'
+	}
+};
