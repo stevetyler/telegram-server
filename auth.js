80c2f974bbae814d0b78bb13302b371f5b06efdc
Steve Tyler <mail@steve-tyler.co.uk>
2015-03-16 21:23:08 +0000

    Updated callback URL

--- auth.js
+++ auth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'twitterAuth' : {
 		'consumerKey': 'qV8AhrTENneEbMaiy8MEOHWfi',
 		'consumerSecret': 'FH0sf3agdNQppopS1ilBHrZXOVPcgH9uBhhWxhq67r4VLlXLa3',
-		'callbackURL': 'http://178.62.92.55/auth/twitter/callback'
+		'callbackURL': 'http://www.telegram.com/api/users/auth/twitter/callback'
 	}
 };
