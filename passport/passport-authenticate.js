e9e7633896055741f0d03ffe7508cde30d6f36a4
Steve Tyler <mail@steve-tyler.co.uk>
2015-03-16 18:29:47 +0000

    Added Twitter strategy

--- passport/passport-authenticate.js
+++ passport/passport-authenticate.js
@@ -3,6 +3,8 @@ var db = require('./../database/database');
 var LocalStrategy = require('passport-local').Strategy;
 var logger = require('nlogger').logger(module);
 var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
+var configAuth = require('./../auth'); // import Twitter consumer key & secret
 
 var User = db.model('User');
 
@@ -33,6 +35,43 @@ passport.use(new LocalStrategy(
   }
 ));
  
+passport.use(new TwitterStrategy({
+    // pull in our app consumer key and secret from auth.js file
+    consumerKey: configAuth.twitterAuth.consumerKey,
+    consumerSecret: configAuth.twitterAuth.consumerSecret,
+    callbackURL: configAuth.twitterAuth.callbackURL
+  },
+  // twitter will send back token and profile
+  function(token, tokenSecret, profile, done) {
+    User.findOne({ id: profile.id }, function(err, user) {
+      logger.info('user found from twitter: ', profile);
+      if(err) {
+        done(err);
+      }
+      if(user) {
+        return done(null, user); // if user is found, log them in
+      } else {
+
+        var newUser = {};
+
+        // newUser.id = profile.id;
+
+
+        User.create(newUser, function(err, user) {
+          if (err) {
+            logger.error('User not Created', err);
+            done(err);
+          }
+          logger.info('User Created: ', user.id);
+          return done(null, user);
+        });
+      }
+    });
+  })
+);
+
+
+
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
